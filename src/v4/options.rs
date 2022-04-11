8ee11699a9678f0b1791c58824ea0af845e1be4f
Evan Cameron <ecameron@bluecatnetworks.com>
2022-04-11 17:02:50 -0400

    dhcpv4: fix unknown opt

--- src/v4/options.rs
+++ src/v4/options.rs
@@ -847,7 +847,7 @@ impl Decodable for DhcpOption {
                 RelayAgentInformation(relay::RelayAgentInformation::decode(&mut dec)?)
             }
             OptionCode::ClientLastTransactionTime => {
-                let _ = decoder.read_u8()?;
+                let _ = decoder.read_u8()?; // always 4
                 ClientLastTransactionTime(decoder.read_u32()?)
             }
             OptionCode::AssociatedIp => {
@@ -1303,10 +1303,10 @@ mod tests {
     fn test_unknown() -> Result<()> {
         test_opt(
             DhcpOption::Unknown(UnknownOption {
-                code: 91,
+                code: 97,
                 data: vec![1, 2, 3, 4],
             }),
-            vec![91, 4, 1, 2, 3, 4],
+            vec![97, 4, 1, 2, 3, 4],
         )?;
 
         Ok(())
