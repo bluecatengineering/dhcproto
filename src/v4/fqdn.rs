5ab077b9b45d77d7858aff3a814f4aac89167588
Evan Cameron <ecameron@bluecatnetworks.com>
2022-12-21 13:13:53 -0500

    add Domain::new method

--- src/v4/fqdn.rs
+++ src/v4/fqdn.rs
@@ -76,7 +76,7 @@ impl fmt::Debug for FqdnFlags {
 
 impl fmt::Display for FqdnFlags {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
