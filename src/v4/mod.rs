0d779dfe2e2b236f18e6fc217e17431b6a0e073a
andrew-rj <andrew.spoden@redjack.com>
2025-07-10 16:33:52 -0500

    Added test for 'set_chaddr' with more than 16 characters.

--- src/v4/mod.rs
+++ src/v4/mod.rs
@@ -613,6 +613,9 @@ mod tests {
         );
         msg.set_chaddr(&[0, 1, 2, 3, 4, 5]);
         assert_eq!(msg.chaddr().len(), 6);
+
+        msg.set_chaddr(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]);
+        assert_eq!(msg.chaddr().len(), 16);
         Ok(())
     }
 
